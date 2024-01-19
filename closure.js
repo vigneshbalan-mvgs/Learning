003261ffe9ea089c70e52137765b0322ad538362
vigneshbalan-mvgs <vigneshbalanmvgs2003@gmail.com>
2024-01-19 21:14:21 +0530

    Create Closure.js

---
+++ closure.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let count = n
+    return function() {
+        count++
+        return count-1
+    };
+};
+
+
+ 
+// const counter = createCounter(10)
+// counter() // 10
+// counter() // 11
+// counter() // 12
+
+
+//**
+//it determine the closure function and howThe code you've provided defines a function `createCounter` that takes a number `n` as an argument and returns a function `counter`. This returned function, when invoked, increments a counter variable (`count`) and returns the incremented value.
+
+
