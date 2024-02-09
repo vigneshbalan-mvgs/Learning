990efbf1f1a93acbcddae8af6fb72b01b3d07c2f
vigneshbalan-mvgs <vigneshbalanmvgs2003@gmail.com>
2024-02-09 21:58:17 +0530

    changes

---
+++ sleep.js
@@ -0,0 +1,21 @@
+function hello() {
+   console.log('hello');
+}
+
+setTimeout(hello,1000);
+setTimeout(hello,2000);
+//promise are mainly focus on 3 things
+//Pending 
+//Resolved
+//Rejected
+
+new Promise();
+
+function callback(resolve, reject){
+  //Async operation 
+
+  //network call disk reat I/O
+  //
+  //sitting idle
+  //single threaded
+}
