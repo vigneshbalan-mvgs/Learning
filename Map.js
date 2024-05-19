f8e028285f049a08d2e6d61e8b206d6d62f93939
vignesh mvgs <vigneshbalanmvgs2003@gmail.com>
2024-05-19 15:57:06 +0530

    small changes

--- Map.js
+++ Map.js
@@ -1,21 +1,22 @@
 //Map is help to reduce the stress in big data
 
-let map =  new Map();
+let map = new Map();
 
 map.set("Navin", "java");
-map.set("Kiran", "Andriod");
+map.set("Kiran", "Android");
+map.set("hello", "everyone");
 map.set("Chirag", "ML");
 map.set("Navin", "BlockChain");
 
-console.log(map.keys()); // keys is the values that can help to print the value 
+console.log(map.keys()); // Keys is the values that can help to print the value 
 
 console.log(map.get('Chirag'));
 
-for (let [k,v] of map) { // it means we have 2 map values so that we have to chosse the 
+for (let [k, v] of map) { // It means we have 2 map values so that we have to choose the 
   //two different values & then we console log with K,V like that 
-  console.log(k,":",v);
+  console.log(k, ":", v);
 }
 
-map.forEach((v,k) => { // we use K,V in for and then we use v,k in foreach so print everything
-   console.log(k,":", v);
+map.forEach((v, k) => { // We use K,V in for and then we use v,k in for each so print everything
+  console.log(k, ":", v);
 });
